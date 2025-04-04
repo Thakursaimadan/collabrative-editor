ad168c9ff5ca357680e2a2ebce08b423a0bf4e5c
Thakur Sai Madan <thakursaimadan@gmail.com>
2025-04-04 19:11:54 +0530

    resolved some frontend issues

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate ,Link} from "react-router-dom";
 import axios from "axios";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
